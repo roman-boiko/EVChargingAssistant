c6b9ec45783fcc18f94737ff6b91fa9831598bed
roman-boiko <romboiko@amazon.com>
2024-09-23 17:55:43 +0200

    chat and map

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,48 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-import { Authenticator } from "@aws-amplify/ui-react";
+// import type { Schema } from "../amplify/data/resource";
+// import { generateClient } from "aws-amplify/data";
 import '@aws-amplify/ui-react/styles.css';
-
-const client = generateClient<Schema>();
+import { useState } from 'react';
+import { MapView } from '@aws-amplify/ui-react-geo';
+import '@aws-amplify/ui-react-geo/styles.css';
+import { Marker } from 'react-map-gl'; 
+// const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({id});
-  }
+  const [isToggled, setIsToggled] = useState(false);
+  const [{ latitude, longitude }, setMarkerLocation] = useState({
+    latitude: 48.8566,
+    longitude: 2.3522,
+  });
+  const updateMarker = () =>
+    setMarkerLocation({ latitude: latitude + 5, longitude: longitude + 5 });
 
   return (
-    <Authenticator>
-      {({ signOut }) => (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id} onClick={() => deleteTodo(todo.id)}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
+
+    <main style={{ display: "flex", flexDirection: "row", height: "100vh", width: "100vw" }}>
+      <div style={{ flex: 3, padding: "10px", height: "50%" }}>
+        <MapView style={{ height: "100%", width: "100%" }} initialViewState={{ longitude: 2.3522, latitude: 48.8566, zoom: 12 }}>
+          <Marker longitude={longitude} latitude={latitude} color="red" />
+        </MapView>
       </div>
-      <button onClick={signOut}>Sign out</button>
+      <div style={{ flex: 1, padding: "10px", display: "flex", flexDirection: "column" }}>
+        <div>
+          <div style={{ display: "flex", alignItems: "center" }}>
+           
+            <p>Travel Advisor</p>
+            <button style={{ marginLeft: "10px" }} onClick={() => setIsToggled(!isToggled)}>
+              <span role="img" aria-label="microphone">{isToggled ? "🎤" : "🔇"}</span>
+            </button>
+          </div>
+          <div style={{ borderBottom: "3px solid black", padding: "10px", height: "300px", width: "100%", overflowY: "scroll", flex: 1, marginBottom: "10px" }}>
+            <p>Output text field</p>
+          </div>
+          <input type="text" placeholder="Type your message here" style={{ width: "100%", marginBottom: "10px" }} />
+          <button style={{ width: "100%" }}>Submit</button>
+        </div>
+      </div>
     </main>
-    )}
-    </Authenticator>
   );
+
 }
 
 export default App;
