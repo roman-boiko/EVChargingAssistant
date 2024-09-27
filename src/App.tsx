f62c6ceea0c5d7d146cdbce8312d9d87b8d721a8
roman-boiko <romboiko@amazon.com>
2024-09-27 09:14:18 +0200

    prep frontend app

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,19 @@ function App() {
     latitude: 48.8566,
     longitude: 2.3522,
   });
-  const updateMarker = () =>
-    setMarkerLocation({ latitude: latitude + 0.01, longitude: longitude + 0.01 });
+  const [{ chargingOneLatitude, chargingOneLongitude, chargingOneOpacity }, setChargingOneLocation] = useState({
+    chargingOneLatitude: 0,
+    chargingOneLongitude: 0,
+    chargingOneOpacity: 0
+  })
+  const [{ chargingTwoLatitude, chargingTwoLongitude, chargingTwoOpacity }, setChargingTwoLocation] = useState({
+    chargingTwoLatitude: 0,
+    chargingTwoLongitude: 0,
+    chargingTwoOpacity: 0
+  })
+  const [chatMessages, setChatMessages] = useState<string[]>([]);
+  const [inputText, setInputText] = useState<string>("");
+
 
   useEffect(() => {
     client.subscriptions.onCarLocationUpdate().subscribe({
@@ -33,7 +44,15 @@ function App() {
     <main style={{ display: "flex", flexDirection: "row", height: "100vh", width: "100vw" }}>
       <div style={{ flex: 3, padding: "10px", height: "50%" }}>
         <MapView style={{ height: "100%", width: "100%" }} initialViewState={{ longitude: 2.3522, latitude: 48.8566, zoom: 12 }}>
-          <Marker longitude={longitude} latitude={latitude} color="red" />
+          <Marker longitude={longitude} latitude={latitude}>
+            <div style={{ fontSize: '24px' }}>🚗</div>
+          </Marker>
+          <Marker longitude={chargingOneLongitude} latitude={chargingOneLatitude} style={{ opacity: chargingOneOpacity }}>
+            <div style={{ fontSize: '24px' }}>🔋</div>
+          </Marker>
+          <Marker longitude={chargingTwoLongitude} latitude={chargingTwoLatitude} style={{ opacity: chargingTwoOpacity }}>
+            <div style={{ fontSize: '24px' }}>🔋</div>
+          </Marker>
         </MapView>
       </div>
       <div style={{ flex: 1, padding: "10px", display: "flex", flexDirection: "column" }}>
@@ -46,9 +65,15 @@ function App() {
             </button>
           </div>
           <div style={{ borderBottom: "3px solid black", padding: "10px", height: "300px", width: "100%", overflowY: "scroll", flex: 1, marginBottom: "10px" }}>
-            <p>Output text field</p>
+            <p>{chatMessages}</p>
           </div>
-          <input type="text" placeholder="Type your message here" style={{ width: "100%", marginBottom: "10px" }} />
+          <input
+            type="text"
+            placeholder="Type your message here"
+            style={{ width: "100%", marginBottom: "10px" }}
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+          />
           <button style={{ width: "100%" }}>Submit</button>
         </div>
       </div>
